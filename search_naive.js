b186bedc7e036d6aeedcb65dd1c902da60526e24
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2022-08-23 11:17:39 +0100

    Add files via upload

---
+++ search_naive.js
@@ -0,0 +1,16 @@
+// Divide and Conquer
+// This pattern involves dividing a data set into smaller chunks and then repeating a process with a subset of data.
+// This pattern can tremendously decrease time complexity.
+
+// Que: Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located. If the value is not found, return -1. 
+
+function search(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+search([1,2,3,4,5],3)
