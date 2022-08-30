c539afe3fb51b4e7e47abec27bf11c4194fed856
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2022-08-30 21:44:36 +0100

    Add files via upload

---
+++ challenges/section_one/GuessGame.cs
@@ -0,0 +1,40 @@
+using System.Text;
+using System.Threading Tasks;
+
+namespace GuessGame
+{
+ class Program 
+  {
+     static void Main(string[] args)
+   {
+     string secretWord = "coding"
+     string guess = "";
+     int guessCount = 0;
+     int guessLimit = 3;
+     bool outOfGuess = false;
+
+     while (guess != secretWord && !outOfGuess)
+     {
+      if (guessCount < guessLimit)
+      {
+        Console.Write("Enter guess: ");
+        guess = Console.ReadLine();
+        guessCount++;
+      }
+      else
+      {
+        outOfGuess = true;
+      }
+     }
+     if (outOfGuess)
+     {
+      Console.Write("You Lose!");
+     }
+     else
+     {
+      Console.Write("You Win!");
+     }
+    }
+  }
+}
+
